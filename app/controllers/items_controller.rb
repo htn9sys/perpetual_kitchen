822fae609f696b43b55bf64a420aa0a98461125b
htn9sys <hatsune.927@gmail.com>
2023-06-01 01:38:46 +0900

    ログイン_新規登録の分岐画面を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    if !user_signed_in?
-      redirect_to new_user_session_path
+    unless user_signed_in?
+      redirect_to tops_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :tops, only: [:index]
   resources :items
   resources :kitchens
   resources :gardens
