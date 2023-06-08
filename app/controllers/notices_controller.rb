426a147ccb6f14018f6e4be8733a689b44f3e520
htn9sys <hatsune.927@gmail.com>
2023-06-08 12:43:23 +0900

    お知らせページを日付順にソート

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,9 +1,11 @@
 class NoticesController < ApplicationController
   def index
     @items = Item.where("date <= ?", Date.today + 7.days)
-    @today_calendar = Calendar.find_by(start_date: Date.today)
-    @one_week_later_calendar = Calendar.find_by(start_date: Date.today + 7.days)
     @today = Date.today
     @one_week_later = Date.today + 7.days
+
+    @calendars = Calendar.where(start_date: [@today, @one_week_later])
+    @today_calendars = @calendars.select { |calendar| calendar.start_date == @today }
+    @one_week_later_calendars = @calendars.select { |calendar| calendar.start_date == @one_week_later }
   end
 end
